f46687ea4c6084a655f114f90a4bb6c101093bf2
NET <NET@DESKTOP-FFDDU8Q>
2024-05-04 17:10:35 -0300

    arreglos

--- tp2_BarrientosLautaro/src/ar/edu/unju/fi/ejercicio5/model/PagoEfectivo.java
+++ tp2_BarrientosLautaro/src/ar/edu/unju/fi/ejercicio5/model/PagoEfectivo.java
@@ -1,18 +1,52 @@
 package ar.edu.unju.fi.ejercicio5.model;
 
+import java.time.LocalDate;
+
 import ar.edu.unju.fi.ejercicio5.interfaces.Pago;
 
 public class PagoEfectivo implements Pago {
+	private double montoPagado;
+	private LocalDate fechaDePago;
+	
+	public PagoEfectivo(double montoPagado, LocalDate fechaDePago) {
+		this.montoPagado = montoPagado;
+		this.fechaDePago = fechaDePago;
+	}
+	
+	public double getMontoPagado() {
+		return montoPagado;
+	}
+
+
+
+	public void setMontoPagado(double montoPagado) {
+		this.montoPagado = montoPagado;
+	}
+
+
+
+	public LocalDate getFechaDePago() {
+		return fechaDePago;
+	}
+
+
+
+	public void setFechaDePago(LocalDate fechaDePago) {
+		this.fechaDePago = fechaDePago;
+	}
+
+
 
 	@Override
 	public void realizarPago(double monto) {
-		// TODO Auto-generated method stub
+		this.montoPagado -= monto * 0.10;
 
 	}
 
 	@Override
 	public void imprimirRecibo() {
-		// TODO Auto-generated method stub
+		 System.out.println("Fecha de pago: " + fechaDePago);
+	        System.out.println("Monto pagado: " + montoPagado);
 
 	}
 
